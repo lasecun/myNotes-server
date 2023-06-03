0c30a916e798a9a714a438ea8a724df99f77be5e
Juanjo Marti <juanjo.marti@colba.es>
2023-06-03 11:31:49 +0200

    CRUD

--- src/main/kotlin/com/itram/plugins/Routing.kt
+++ src/main/kotlin/com/itram/plugins/Routing.kt
@@ -1,14 +1,85 @@
 package com.itram.plugins
 
+import com.itram.model.Note
 import com.itram.repositories.NotesRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/notes") {
-            call.respond(NotesRepository.getAll())
+        route("/notes") {
+
+            // CREATE
+
+            post {
+                try {
+                    val note = call.receive<Note>()
+                    val savedNote = NotesRepository.save(note)
+                    call.respond(HttpStatusCode.Created, savedNote)
+                } catch (e: Exception) {
+                    call.respond(
+                        HttpStatusCode.BadRequest, "Bad JSON data body ${e.message}"
+                    )
+                }
+            }
+
+            // READ
+            get {
+                call.respond(NotesRepository.getAll())
+            }
+
+            get("{id}") {
+                val id = call.parameters["id"] ?: return@get call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed Id"
+                )
+                val note = NotesRepository.getById(id.toLong()) ?: return@get call.respond(
+                    HttpStatusCode.NotFound,
+                    "No note with id $id"
+                )
+                call.respond(note)
+            }
+
+            // UPDATE
+            put {
+                try {
+                    val note = call.receive<Note>()
+                    if (NotesRepository.update(note)) {
+                        call.respond(note)
+                    } else {
+                        call.respond(
+                            HttpStatusCode.NotFound,
+                            "No note with id ${note.id}"
+                        )
+                    }
+                } catch (e: Exception) {
+                    call.respond(
+                        HttpStatusCode.BadRequest, "Bad JSON data body ${e.message}"
+                    )
+                }
+            }
+
+            // DELETE
+
+            delete("{id}") {
+                val id = call.parameters["id"] ?: return@delete call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed Id"
+                )
+
+                if (NotesRepository.delete(id.toLong())) {
+                    call.respond(HttpStatusCode.Accepted)
+                } else {
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        "No note with id $id"
+                    )
+                }
+            }
+
         }
     }
 }
--- src/main/kotlin/com/itram/repositories/NotesRepository.kt
+++ src/main/kotlin/com/itram/repositories/NotesRepository.kt
@@ -1,20 +1,31 @@
 package com.itram.repositories
 
 import com.itram.model.Note
-import com.itram.model.Note.Type.AUDIO
-import com.itram.model.Note.Type.TEXT
 
 object NotesRepository {
 
-    fun getAll(): List<Note> {
-        val notes = (1..10).map {
-            Note(
-                title = "Title $it",
-                description = "Description $it",
-                if (it % 3 == 0) AUDIO else TEXT
-            )
-        }
+    private val list = mutableListOf<Note>()
+    private var currentId = 1L
 
-        return notes
-    }
+    fun getAll(): List<Note> = list
+
+    fun save(note: Note): Note =
+        note.copy(id = currentId++)
+            .also(list::add)
+
+
+    fun getById(id: Long): Note? = list.find { it.id == id }
+
+    fun update(note: Note): Boolean =
+        list.indexOfFirst { it.id == note.id }
+            .takeIf { it > 0 }
+            ?.also { list[it] = note }
+            .let { it != null }
+
+
+    fun delete(id: Long): Boolean =
+        list.indexOfFirst { it.id == id }
+            .takeIf { it > 0 }
+            ?.also(list::removeAt)
+            .let { it != null }
 }
